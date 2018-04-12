2ad881cab7c8a7efda12d37141786db0adca6cd6
Iker Zamora <ikerzamora94@gmail.com>
2018-04-12 22:10:06 +0200

    Update bot

--- bot.py
+++ bot.py
@@ -4,14 +4,19 @@ import os
 import sys
 import telebot
 
-token = os.environ['TELEGRAM_TOKEN']
+from events import Event, Events
+
+try:
+    token = os.environ['TELEGRAM_TOKEN']
+except KeyError:
+    print("Please set the environment variable TELEGRAM_TOKEN")
+    sys.exit(1)
 
 bot = telebot.TeleBot(token)
 
 commands = {
 # command description used in the 'ayuda' command, keep these up to date
     'ayuda': 'Obtener información acerca de los comandos',
-    'fotopizza': 'Fotopizza de los pizzeros (Próximamente)',
     'hype': 'Tiempo restante para la próxima EE ó AE ó GE ó Gamergy (Próximamente)'
 }
 
@@ -26,18 +31,30 @@ def command_help(m):
         help_text += commands[key] + "\n"
     bot.send_message(cid, help_text)  # send the generated help page
 
-# TODO
-# Returns the fotopizza of the user requested
-@bot.message_handler(commands=['fotopizza'])
-def command_fotopizza(m):
-    cid = m.chat.id
-    bot.send_message(cid, 'Comando en desarrollo')
-
-# TODO
 # Returns the remaining time for the event requested
 @bot.message_handler(commands=['hype'])
 def command_hype(m):
     cid = m.chat.id
-    bot.send_message(cid, 'Comando en desarrollo')
+    param = ""
+    try:
+        param = m.text.split()[1].lower()
+    except IndexError:
+        bot.send_message(
+            cid, "Se necesita un atributo. Uso: /hype (EE | GE | AE)")
+        return
+    events = Events()
+    event = events.get_event(param)
+    if event == 0:
+        bot.send_message(
+            cid, "Ese evento no existe. Uso: /hype (EE | GE | AE)")
+        return
+    days, hours, minutes, seconds = event.time_left()
+    text = "Tiempo restante para la %s%d " % (
+    event.acronym, event.edition)
+    text += "(%d-%d-%d):\n" % (
+        event.date.year, event.date.month, event.date.day)
+    text += " %d días, %d horas, %d minutos y %d segundos" % (
+        days, hours, minutes, seconds)
+    bot.send_message(cid, text)
 
 bot.polling()
---
+++ events.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import json
+
+class Event:
+	
+	def __init__(self, acronym, date, first_edition_year, name):
+		self.acronym = acronym
+		self.date = date
+		self.first_edition_year = first_edition_year
+		self.name = name
+		self.edition = self.date.year - self.first_edition_year + 1
+
+	def time_left(self):
+		'''
+		Return: days, hours, minutes and seconds
+		        remaining for the next LAN party
+		'''
+		now = datetime.now()
+		td = self.date - now
+		days = td.days
+		hours = td.seconds // 3600
+		minutes = (td.seconds // 60) % 60
+		seconds = (td.seconds % 3600) % 60
+		return days, hours, minutes, seconds
+
+class Events:
+
+	def __init__(self):
+		self.events = []
+
+	def _load_events(self, filename = 'events.json'):
+		file = open(filename)
+		data = json.load(file)
+		file.close()
+		json_events = data['events']
+		for event in json_events:
+			current = event['event']
+			year = current['date']['year']
+			month = current['date']['month']
+			day = current['date']['day']
+			hour = current['date']['hour']
+			minute = current['date']['minute']
+			date = datetime(year,month,day,hour,minute)
+			current_event = Event(current['acronym'], date, current['first_edition_year'], current['name'])
+			self.events.append(current_event)
+
+	def get_event(self, acronym):
+		self._load_events()
+		for event in self.events:
+			if event.acronym.lower() == acronym.lower():
+				return event
+		return 0
